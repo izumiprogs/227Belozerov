3fce68f1a2d6a388a971a392a16641204ada7435
Ярослав <146277976+izumiprogs@users.noreply.github.com>
2023-12-04 10:42:03 +0300

    выполненные задания

---
+++ 227/2272/2272/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace _2272
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            void BtnOK_Click(object sender, EventArgs e)
+            {
+                double a = Convert.ToDouble(TbNumberA.Text);
+                double b = Convert.ToDouble(TbNumberB.Text);
+                double c = Convert.ToDouble(TbNumberC.Text);
+                double s;
+                s = Abs(a - c) * Abs(b - c);
+                TextBlockAnswer.Text = ($" s={s}");
+
+            }
+        }
+    }
+}
---
+++ 227/2273/2273/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+usin
+
+namespace _2273
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            void BtnOK_Click(object sender, EventArgs e)
+            {
+                double v = Convert.ToDouble(TbNumberV.Text);
+                double u = Convert.ToDouble(TbNumberU.Text);
+                double t1 = Convert.ToDouble(TbNumberT1.Text);
+                double t2 = Convert.ToDouble(TbNumberT2.Text);
+                double Inf = Convert.ToDouble(TbNumberInf.Text);
+                double s;
+                if (t2 == 0) { s = (v + u) * t1; }
+                else { s = t1 * v + t2 * (v - u); }
+                TextBlockAnswer.Text = ($" s={s}");
+
+            }
+        }
+    }
+}
